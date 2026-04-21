487e117d2080fd49c38eca7384484de750acdc1c
Jose Manuel Vicente Checo <josemvicente22@gmail.com>
2026-04-21 15:04:59 -0400

    Prueba final CI/CD

--- HolaMundoDocker/Program.cs
+++ HolaMundoDocker/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hola Mundo desde C# con Docker, GitHub Actions y Render");
+app.MapGet("/", () => "Hola Mundo desde C# con Docker, GitHub Actions y Render, 🔥 PIPELINE FUNCIONANDO AUTOMATICO 🔥");
 
 app.Run();
